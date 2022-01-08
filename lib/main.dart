0bf9da1aed88fa62e46689ecbc8f00c596f68425
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2022-01-08 18:26:36 -0400

    Construir arbor de directorios bajo el patron clean architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:qard_wallet/screem/home_screem.dart';
+
+import 'presentation/screem/home_screem.dart';
 
 void main() {
   runApp(const MyApp());  
